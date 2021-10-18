5547791ccd480346587aa130014af165c1aa9ddb
Nikita Kozlovtsev <Nikita_Kozlovtsev@dietsmann.com>
2021-10-18 11:41:21 +0300

    update admin notifications

--- app/telegram_config/utils/notify_admins.py
+++ app/telegram_config/utils/notify_admins.py
@@ -7,7 +7,7 @@ from telegram_config.data.config import ADMIN_ID
 
 async def on_startup_notify(dp: Dispatcher):
     try:
-        await dp.bot.send_message(ADMIN_ID, "Бот Запущен")
+        await dp.bot.send_message(ADMIN_ID, "Бот Запущен! /start")
 
     except Exception as err:
         logging.exception(err)
